0c1377eab3bbcb082bef2518c50943f783015359
Денис <mden1996.07@gmail.com>
2024-03-15 22:53:14 +0300

    prob done

--- Task/Program.cs
+++ Task/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 
-namespace Task
+namespace TaskApp
 {
     class Program
     {
@@ -14,27 +14,39 @@ namespace Task
         {
             Console.WriteLine("Введите размер массива случайных чисел:");
             int n = Convert.ToInt32(Console.ReadLine());
+            Func<object, int[]> func1 = new Func<object, int[]>(CreateArray);    
+            Task<int[]> task1 = new Task<int[]>(func1,n);
 
 
-            Task task = new Task();
+            Action<Task<int[]>> act1 = new Action<Task<int[]>>(PrintArr);
+            Task task2 = task1.ContinueWith(act1);
 
+            Func<Task<int[]>, int> func2 = new Func<Task<int[]>, int>(ArraySumm);
+            Task<int> task3 = task1.ContinueWith<int>(func2);
 
-
-
+            Func<Task<int[]>, int> func3 = new Func<Task<int[]>, int>(ArrayMax);
+            Task<int> task4 = task1.ContinueWith<int>(func3);
+            task1.Start();
+            task2.Wait();
+            Console.WriteLine(task3.Result);
+            Console.WriteLine(task4.Result);
             Console.ReadKey();
 
         }
 
-        static int ArraySumm(int[] arr)
+
+        static int ArraySumm(Task<int[]> task)
         {
+            int[] arr = task.Result;
             int summ = 0;
             foreach(int a in arr) {
                 summ += a;
             }
             return summ;
         }
-        static int ArrayMax(int[] arr)
+        static int ArrayMax(Task<int[]> task)
         {
+            int[] arr = task.Result;
             int max = arr[0];
             foreach(int a in arr) {
                 if(max<a) {
@@ -43,17 +55,19 @@ namespace Task
             }
             return max;
         }
-        static int[] CreateArray(int n)
+        static int[] CreateArray(object a)
         {
+            int n = (int)a;
             Random random = new Random();
             int[] array = new int[n];
             for(int i = 0; i < n; i++) {
-                array[i] = random.Next(-10000, 10000);
+                array[i] = random.Next(0, 1000);
             }
             return array;
         }
-        static void PrintArr(int[] array)
+        static void PrintArr(Task<int[]> task)
         {
+            int[] array = task.Result;
             foreach(int a in array) {
                 Console.Write($"{a} ");
             }
